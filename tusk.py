f3ef1faf0b5c3f05dc49003fb1a90914ceb3749a
katia29 <hol.kat12@gmail.com>
2024-07-20 18:02:09 +0300

    dfrg

--- tusk.py
+++ tusk.py
@@ -8,7 +8,7 @@ class Widget():
         print("інформація про віджет")
         print(self.text,self.x,self.y)
 class Button(Widget):
-    def __init__(self, text, x, y, is_cklick):
+    def __init__(self, text, x, y, is_click):
         super().__init__(text, x, y)
         self.is_click = is_click
     
