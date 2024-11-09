2f987dbaba9b9322df15f9f0c84b89f159629215
katia29 <120781728+katia29@users.noreply.github.com>
2024-11-09 17:29:35 +0200

    Add files via upload

--- notes/main.py
+++ notes/main.py
@@ -11,7 +11,13 @@ class Widget(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         for note in notes.keys():
-            self.ui.list_1.addItem(note)
+            self.ui.list1.addItem(note)
+        self.ui.list1.itemDoubleClicked.connect(self.show_note)
+        self.ui.btn_add.clicked.connect(self.add_tag)
+    def show_note(self):
+        self.ui.list1.clear()
+        self.ui.list2.clear()
+        self.ui.list_1.addItem(note)
         self.ui.list_1.itemClicked.connect(self.show_note)
         self.ui.btn_make.clicked.connect(self.add_note)
         self.ui.btn_save.clicked.connect(self.safe_notes)
@@ -23,6 +29,18 @@ class Widget(QMainWindow):
         self.ui.textEdit.clear()
         note_name = item.text()
         if note_name in notes:
+            self.ui.list2.addItem(notes[note_name]["теги"])
+            self.ui.lineEdit.setText(notes[note_name]["текст"])
+
+    def add_tag(self):
+    
+        if self.ui.list1.currentItem():
+            tag_name,ok =QInputDialog.getText(self,"Додати тег","Введіть тег")
+            if tag_name and ok:
+               
+                note_name =self.ui.list1.currentItem().text()
+                if note_name in notes[note_name]["теги"]:
+                    self.ui.list2.addItem(tag_name)
             self.ui.list_2.addItems(notes[note_name]["теги"])
             self.ui.textEdit.setText(notes[note_name]["текст"])
 
@@ -54,7 +72,7 @@ class Widget(QMainWindow):
         if self.ui.list_1.currentItem():
             note_name = self.ui.list_1.currentItem().text()
             del notes[note_name]
-            self.ui.list_1.takeItem(self.ui.list_1.currentRow())
+            self.ui.list1.takeItem(self.ui.list1.currentRow())
         self.write_to_file()
 
 
@@ -62,7 +80,7 @@ class Widget(QMainWindow):
         note_name,ok = QInputDialog.getText(self , "Додати замітку" , "Назва замітки")
         if ok and note_name != "":
             notes[note_name] = {"теги": [], "текст":""}
-            self.ui.list_1.addItem(note_name)
+            self.ui.list1.addItem(note_name)
         self.write_to_file()
     def write_to_file(self):
         with open("notes/notes.json","w",encoding="utf-8") as file:
